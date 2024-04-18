5ff34edf8a7849513992328c363c37ba5c0a33b5
MercMayhem <amanrao032@gmail.com>
2024-04-19 01:27:57 +0530

    finished setup page 2

--- src/pages/components/setup/choice.jsx
+++ src/pages/components/setup/choice.jsx
@@ -1,7 +1,22 @@
+import logo from '../../assets/flair_image.png'
 
-function Choice({children, heading}){
+function Choice({children, heading, selected, setSelected, image}){
+    const clickHandler = () => {
+        if (selected) {
+            setSelected(false)
+        } else {
+            setSelected(true)
+        }
+    }
+
     return(
-        <div className="choice">{children}</div>
+        <div className={`choice ${selected ? "selected" : "not-selected"}`} onClick={ clickHandler }>
+            <div className={`heading-box ${selected ? "raise" : ""}`}>
+                <div><img src={ image } style={{ height: "110px", width: "auto" }}/></div>
+                <h3 className="heading" style={{marginTop:"3px"}}>{ heading }</h3>
+                <p className={`contents ${selected ? 'raise' : ""}`}>{ children }</p>
+            </div>
+        </div>
     )
 }
 

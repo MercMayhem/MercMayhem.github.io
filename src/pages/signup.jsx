bf4efac96f70aa43d33c2543853062ddee51b256
MercMayhem <amanrao032@gmail.com>
2024-04-19 01:27:22 +0530

    fixed password bug

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 
 function Signup(){
     const [email, setEmail] = useContext(UserContext)
-    const [password, setPassword] = useState(false)
+    const [password, setPassword] = useState("")
     let navigate = useNavigate();
 
     let submitHandler = (e) => {
